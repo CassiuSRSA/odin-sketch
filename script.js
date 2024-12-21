a3ad86396f3b1f7cb7980b2445b2497c93a86d9b
CassiuSRSA <s.brookstein@gmail.com>
2024-12-21 12:48:59 +0200

    Core project completed

--- script.js
+++ script.js
@@ -1,14 +1,30 @@
 const container = document.getElementById("container");
+const btn = document.querySelector("button");
+let sketchSize = 16;
 
-function makeRows(rows, cols) {
-  for (i = 0; i < rows * cols; i++) {
+btn.addEventListener("click", (e) => {
+  e.preventDefault();
+  sketchSize = prompt("What size sketch do you want (max 100)");
+  while (sketchSize < 1 || sketchSize > 100) {
+    sketchSize = prompt(
+      "Please choose a positive number less than 100 (max 100)"
+    );
+  }
+  makeRows(sketchSize);
+  const flexItems = document.querySelectorAll(".flex-item");
+  flexItems.forEach((item) => {
+    console.log((item.style.width = 960 / sketchSize + "px"));
+    item.style.height = 960 / sketchSize + "px";
+  });
+});
+
+function makeRows(size) {
+  container.innerHTML = "";
+  for (i = 0; i < size * size; i++) {
     let cell = document.createElement("div");
-    cell.innerText = i + 1;
     container.appendChild(cell).className = "flex-item";
   }
 }
-
-makeRows(16, 16);
 
 container.addEventListener("mouseover", (e) => {
   if (e.target.classList.contains("flex-item")) {
