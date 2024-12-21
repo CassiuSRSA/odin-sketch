992f3511f712fa4dd33f17b86b56e34f577d59df
CassiuSRSA <s.brookstein@gmail.com>
2024-12-21 09:04:39 +0200

    Added initial grid and hover

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 const container = document.getElementById("container");
 
 function makeRows(rows, cols) {
-  container.style.setProperty("--grid-rows", rows);
-  container.style.setProperty("--grid-cols", cols);
   for (i = 0; i < rows * cols; i++) {
     let cell = document.createElement("div");
     cell.innerText = i + 1;
-    container.appendChild(cell).className = "grid-item";
+    container.appendChild(cell).className = "flex-item";
   }
 }
 
 makeRows(16, 16);
+
+container.addEventListener("mouseover", (e) => {
+  if (e.target.classList.contains("flex-item")) {
+    e.target.classList.add("hovered");
+  }
+});
